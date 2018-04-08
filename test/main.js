7f87cbca9f5128a047367de07149e551428eb57f
Danstan O. Onyango Zemuldo <danstan.otieno@gmail.com>
2018-04-09 00:35:07 +0300

    complete restructure

--- test/main.js
+++ test/main.js
@@ -5,18 +5,22 @@ import momment from 'moment-timezone';
 test('Test initialize the library with no data, error', t => {
 
   let gpsPack = new Main();
-  t.is(gpsPack.validate().error, 'initialized parser with no string');
-  t.is(gpsPack.getEncodingType().isVoid, true);
+  t.deepEqual(gpsPack.poles, []);
+  t.is(gpsPack.isVoid, true);
+  t.is(gpsPack.encoding, 'invalid');
+  t.deepEqual(gpsPack.arrayData, []);
 });
 
 
 test('Test Unknown Encoding Type', t => {
 
-  let data = 'dskfsdjfhsdkjhfsdjkf';
+  let data = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
-  let gpsPack = new Main(data);
-  t.is(gpsPack.getEncodingType().isVoid, true);
-  t.is(gpsPack.getEncodingType().encoding, 'invalid');
+  let gpsPack = new Main();
+  t.deepEqual(gpsPack.poles, []);
+  t.is(gpsPack.isVoid, true);
+  t.is(gpsPack.encoding, 'invalid');
+  t.deepEqual(gpsPack.arrayData, []);
 });
 
 test('Test initialize the library with data, should return the string true', t => {
@@ -24,8 +28,10 @@ test('Test initialize the library with data, should return the string true', t =
   let data = '$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.validate(), true);
-  t.is(gpsPack.getEncodingType().isVoid, false);
+  t.deepEqual(gpsPack.poles, ['N', 'E']);
+  t.is(gpsPack.isVoid, false);
+  t.is(gpsPack.encoding, 'nmea');
+  t.deepEqual(gpsPack.arrayData, data.split(','));
 });
 
 test('Test Nmea Encoding Type', t => {
@@ -33,27 +39,32 @@ test('Test Nmea Encoding Type', t => {
   let data = '$GPRMC,123519,V,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.getEncodingType().isVoid, true);
-  t.is(gpsPack.getEncodingType().encoding, 'nmea');
+  t.is(gpsPack.encoding, 'nmea');
 });
 
-test('Test Nmea Encoding Type', t => {
+test('Test void data NMEA', t => {
 
-  let data = '$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A';
+  let data = '$GPRMC,123519,V,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.getEncodingType().isVoid, false);
-  t.is(gpsPack.getEncodingType().encoding, 'nmea');
+  t.is(gpsPack.isVoid, true);
 });
 
+test('Test void data HQ NMEA', t => {
+
+  let data = '*HQ,6028021806,V1,125601,V,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
+
+  let gpsPack = new Main(data);
+  t.is(gpsPack.isVoid, true);
+});
 
 test('Test HQ Nmea Encoding Type', t => {
 
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.getEncodingType().isVoid, false);
-  t.is(gpsPack.getEncodingType().encoding, 'hqnmea');
+  t.is(gpsPack.isVoid, false);
+  t.is(gpsPack.encoding, 'hqnmea');
 });
 
 test('Test Get Pole orientation', t => {
@@ -61,9 +72,9 @@ test('Test Get Pole orientation', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.getPoles()[0], 'S');
-  t.is(gpsPack.getPoles()[1], 'E');
-  t.is(gpsPack.getPoles()[2], undefined);
+  t.is(gpsPack.poles[0], 'S');
+  t.is(gpsPack.poles[1], 'E');
+  t.is(gpsPack.poles[2], undefined);
 });
 
 test('Test Get Pole orientation', t => {
@@ -71,9 +82,9 @@ test('Test Get Pole orientation', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,N,03654.2240,W,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.getPoles()[0], 'N');
-  t.is(gpsPack.getPoles()[1], 'W');
-  t.is(gpsPack.getPoles()[2], undefined);
+  t.is(gpsPack.poles[0], 'N');
+  t.is(gpsPack.poles[1], 'W');
+  t.is(gpsPack.poles[2], undefined);
 });
 
 test('Test Get decoded data', t => {
@@ -81,9 +92,8 @@ test('Test Get decoded data', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  t.is(gpsPack.getLatitude(gpsPack.getPoles()[0]), -1.21582);
-  t.is(gpsPack.getLongitude(gpsPack.getPoles()[1]), 36.903733333333335);
+  t.is(gpsPack.latitude, -1.21582);
+  t.is(gpsPack.longitude, 36.903733333333335);
 });
 
 test('Test Get Long ', t => {
@@ -91,8 +101,7 @@ test('Test Get Long ', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,N,03654.2240,W,098.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  t.is(gpsPack.getLongitude(), -36.903733333333335);
+  t.is(gpsPack.longitude, -36.903733333333335);
 });
 
 test('Test Get Lat ', t => {
@@ -100,8 +109,7 @@ test('Test Get Lat ', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,N,03654.2240,W,098.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  t.is(gpsPack.getLatitude(), 1.21582);
+  t.is(gpsPack.latitude, 1.21582);
 });
 
 test('Test Get  speed KmH ', t => {
@@ -109,9 +117,7 @@ test('Test Get  speed KmH ', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,N,03654.2240,W,098.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  t.is(gpsPack.getSpeedKmH(), 181.57008000000002);
-  t.is(gpsPack.getSpeedKnots(), 98.04);
+  t.is(gpsPack.speedkmh, 181.57008000000002);
 });
 
 test('Test Get  speed knots ', t => {
@@ -119,51 +125,52 @@ test('Test Get  speed knots ', t => {
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,N,03654.2240,W,098.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
   let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  t.is(gpsPack.getSpeedKnots(), 98.04);
+  t.is(gpsPack.speedknots, 98.04);
+});
+
+test('Test Get Date Time array', t => {
+
+  let data = '*HQ,6028022385,V1,171403,A,0114.5751,S,03639.7117,E,000.02,000,080418,FFFFBBFF,639,02,04104,12176,10,31#';
+
+  let gpsPack = new Main(data);
+  t.deepEqual(gpsPack.dateArray, [2018,4,8,17,14,3]);
+});
+
+test('Test Get Date Time array', t => {
+
+  let data = '*HQ,6028022385,V1,171403,A,0114.5751,S,03639.7117,E,000.02,000,080418,FFFFBBFF,639,02,04104,12176,10,31#';
+
+  let gpsPack = new Main(data);
+  t.deepEqual(gpsPack.ownDate, new Date('2018-04-08 14:14:03 UTC'));
 });
 
 test('Test Get Date Time data', t => {
 
-  let data = '*HQ,6028021806,V1,125601,A,0112.9492,N,03654.2240,W,098.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
-  
+  let data = '*HQ,6028022385,V1,171403,A,0114.5751,S,03639.7117,E,000.02,000,080418,FFFFBBFF,639,02,04104,12176,10,31#';
+
   let gpsPack = new Main(data);
-  let poles = gpsPack.getPoles();
-  t.is(gpsPack.decode(), true);
-  t.deepEqual(gpsPack.getDateISO(), new Date(Date.parse(momment([2018, 2, 17, 15, 56, 1]).tz('Africa/Nairobi').format())));
-  t.is(gpsPack.getDateISO().toISOString(), '2018-03-17T12:56:01.000Z');
-  t.deepEqual(gpsPack.getDateArr(), [2018, 2, 17, 15, 56, 1]);
+  t.deepEqual(gpsPack.date.toISOString(), new Date('2018-04-08 14:14:03 UTC').toISOString());
 });
+
+
+
 
 test('Test Range', t => {
 
   let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
-    
+
   let gpsPack = new Main(data);
-  let poles = gpsPack.getPoles();
-  t.is(gpsPack.decode(), true);
   t.is(gpsPack.getRangeFromKnow(-1.21343, 36.92356), 2.2200929037438093);
 });
 
-test('Test Range', t => {
+// test('Test reverse geocoding google', t => {
+//   let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
 
-  let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
-    
-  let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  gpsPack.reverseGeoLocGoogle(process.env.GOOGLE_MAPS_API_KEY).then(o=>{
-    t.is(o.status,'kljlkj');
-  });
-});
+//   let gpsPack = new Main(data);
+//   t.is(gpsPack.decode(), true);
 
-test('Test reverse geocoding google', t => {
-  let data = '*HQ,6028021806,V1,125601,A,0112.9492,S,03654.2240,E,000.04,000,170218,FFF7BBFF,639,02,04009,12902,7,31#';
-    
-  let gpsPack = new Main(data);
-  t.is(gpsPack.decode(), true);
-  
-  return Promise.resolve(gpsPack.reverseGeoLocGoogle(process.env.GOOGLE_MAPS_API_KEY))
-    .then(o => {
-      t.is(o.constructor, Array);
-    });
-});
+//   return Promise.resolve(gpsPack.reverseGeoLocGoogle(process.env.GOOGLE_MAPS_API_KEY))
+//     .then(o => {
+//       t.is(o.constructor, Array);
+//     });
+// });
